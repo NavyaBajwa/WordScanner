d0a1401e76ec8c3094c7b514ba0fda8a81b3b9c3
Navya Bajwa <navyabajwa25@gmail.com>
2025-10-22 18:13:04 -0400

    Initial Commit

---
+++ Word Scanner/Word_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScannerApp.swift
+//  Word Scanner
+//
+//  Created by Navya Bajwa on 2025-10-22.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
